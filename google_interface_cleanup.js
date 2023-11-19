67f629b6e82a546fe50074c70f3d7dd5ccffbd76
usernomom <148295218+usernomom@users.noreply.github.com>
2023-11-19 15:27:04 -0500

    t

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      8
+// @version      10
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -118,3 +118,4 @@ waitForKeyElements('#kp-wp-tab-overview > div', otherCrap);
 waitForKeyElements('#bres > div', otherCrap);
 waitForKeyElements('#rso > div', otherCrap)
 waitForKeyElements('#iur div[jscontroller]', images)
+waitForKeyElements('#lb', images)
