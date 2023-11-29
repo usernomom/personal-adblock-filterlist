4b798cf95d3087967de7e702498c1e1ef28db1a8
usernomom <148295218+usernomom@users.noreply.github.com>
2023-11-28 22:56:26 -0500

    t

--- instacart_ad_remover.js
+++ instacart_ad_remover.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Instacart Ad Remover
-// @version  12
+// @version  13
 // @match    https://*.instacart.ca/*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
