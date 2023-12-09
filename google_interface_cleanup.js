f718715acf5473f4b0d58d2027b0b138b21bb4c2
usernomom <148295218+usernomom@users.noreply.github.com>
2023-12-09 02:07:13 +0000

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      24
+// @version      25
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -47,7 +47,8 @@ const websitesToBlock = [
     "cp24.com",
     "ctvnews.ca",
     "iranintl.com",
-    "wionews.com"
+    "wionews.com",
+    "calgaryherald.com"
 ]
 
 // Where el is the DOM element you'd like to test for visibility
