3a420a98521dc774b3f5c0dffd3ba13cde067e92
usernomom <148295218+usernomom@users.noreply.github.com>
2024-03-02 20:44:03 -0500

    t

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      29
+// @version      31
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -57,6 +57,26 @@ const websitesToBlock = [
     "timesofisrael.com"
 ]
 
+const annoyances = [
+    'People also ask',
+    'People also search for',
+    'People also search',
+    'Videos',
+    'Short videos',
+    'Refine this search',
+    'Search a song',
+    'Related searches',
+    'Hum to search',
+    'Trending videos',
+    'For context',
+    'Also searched for',
+    'Often searched together',
+    'Others searched',
+    'Local news',
+    'Popular on X',
+    'People also watch'
+]
+
 // Where el is the DOM element you'd like to test for visibility
 function isHidden(el) {
     if (el === null) {
@@ -79,37 +99,46 @@ function getbyXpath(xpath, contextNode) {
 function otherCrap(jNode) {
     let div = jNode[0]
 
-    let annoyances = [
-        'People also ask',
-        'People also search for',
-        'People also search',
-        'Videos',
-        'Short videos',
-        'Refine this search',
-        'Search a song',
-        'Related searches',
-        'Hum to search',
-        'Trending videos',
-        'For context',
-        'Also searched for',
-        'Often searched together',
-        'Others searched',
-        'Local news',
-        'Popular on X'
-    ]
-
-    let matchingAnnoyance =
+    let matchingAnnoyances =
         annoyances
         .filter(annoyance => div.innerHTML.indexOf(annoyance) != -1)
-        .flatMap(a => getbyXpath(`//div[text()='${a}']|//span[text()='${a}']`, div))
-        .find(node => !isHidden(node, div));
+        .flatMap(a => {
+            let k = getbyXpath(`.//div[text()='${a}']|//span[text()='${a}']`, div)
+            console.log(a, k)
+            return k
+        })
+    // .filter(node => !isHidden(node));
 
-    if (matchingAnnoyance) {
-        let hiddenClue = div.querySelector('.U3THc');
+    let MjjYud = document.querySelectorAll('#rso div.MjjYud')
 
-        if ((hiddenClue === null) && !(div.closest('#appbar'))) {
-            div.style.display = 'none';
+    if (MjjYud.length > 1) {
+        if (matchingAnnoyances.length > 0) {
+            div.style.display = 'none'
         }
+    } else {
+        matchingAnnoyances.forEach(matchingAnnoyance => {
+            if (matchingAnnoyance && !isHidden(matchingAnnoyance)) {
+                console.log(div, matchingAnnoyance)
+
+                datavt = matchingAnnoyance.closest('div[data-vt]')
+
+                if (datavt && !(datavt.closest('#appbar'))) {
+                    datavt.style.display = 'none';
+                } else {
+                    dataart = matchingAnnoyance.closest('div[data-art]')
+
+                    if (dataart && !(dataart.closest('#appbar'))) {
+                        dataart.style.display = 'none';
+                    } else {
+                        jsdata = matchingAnnoyance.closest('div[jsdata]')
+
+                        if (jsdata && !(jsdata.closest('#appbar'))) {
+                            jsdata.style.display = 'none';
+                        }
+                    }
+                }
+            }
+        });
     }
 
     if (div.querySelector("#iur") !== null) {
@@ -131,10 +160,46 @@ function clickbaitNews(jNode) {
     }
 }
 
+function otherCrap2(jNode) {
+    let div = jNode[0]
+    // console.log(div)
+
+    let matchingAnnoyance =
+        annoyances
+        .filter(annoyance => div.innerHTML.indexOf(annoyance) != -1)
+        .flatMap(a => {
+            let k = getbyXpath(`.//div[text()='${a}']|//span[text()='${a}']`, div)
+            // console.log(a, k)
+            return k
+        })
+        .find(node => !isHidden(node))
+
+    if (matchingAnnoyance) {
+        // console.log(div, matchingAnnoyance)
+
+        let hiddenClue = div.querySelector('.U3THc');
+
+        if ((hiddenClue === null)) {
+            // jsdata = matchingAnnoyance.closest('div[jsdata]')
+
+            // if (jsdata && !(jsdata.closest('#appbar'))) {
+            //     jsdata.style.display = 'none';
+            // }
+
+            div.style.display = 'none'
+        }
+    }
+
+    if (div.querySelector("#iur") !== null) {
+        div.style.display = 'none';
+    }
+}
+
+
 // waitForKeyElements('g-scrolling-carousel div[jscontroller] a', clickbaitNews)
 waitForKeyElements('div[role="listitem"] a', clickbaitNews)
-waitForKeyElements('#kp-wp-tab-overview > div', otherCrap);
-waitForKeyElements('#bres > div', otherCrap);
+waitForKeyElements('#kp-wp-tab-overview > div', otherCrap3);
+waitForKeyElements('#bres > div', otherCrap2);
 waitForKeyElements('#rso div.MjjYud', otherCrap);
-waitForKeyElements('div[jsname]', otherCrap)
 waitForKeyElements('#iur div[jscontroller]', undesiredElement)
+waitForKeyElements('div[data-abe]', undesiredElement);
