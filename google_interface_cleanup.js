9cf31c720ff58daa1585a8dd6d336e1436476c5d
usernomom <148295218+usernomom@users.noreply.github.com>
2024-03-06 20:35:45 -0500

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      42
+// @version      43
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -58,7 +58,8 @@ const websitesToBlock = [
     "aljazeera.com",
     "arabnews.com",
     "middleeasteye.net",
-    "eurasiantimes.com"
+    "eurasiantimes.com",
+    "scmp.com"
 ]
 
 const annoyances = [
