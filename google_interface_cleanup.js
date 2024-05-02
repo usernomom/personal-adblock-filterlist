7f7c70aa60ee739282f37f999f22cda162839bbf
usernomom <148295218+usernomom@users.noreply.github.com>
2024-05-02 17:38:57 -0400

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      48
+// @version      49
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -66,7 +66,8 @@ const websitesToBlock = [
     "foxbusiness.com",
     "wsj.com",
     "allisrael.com",
-    "middleeastmonitor.com"
+    "middleeastmonitor.com",
+    "reliefweb.int"
 ]
 
 const annoyances = [
