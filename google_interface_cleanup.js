c93b300eb2461c89a4672f1aace368da4c79ecaf
usernomom <148295218+usernomom@users.noreply.github.com>
2024-05-21 18:40:24 -0400

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      56
+// @version      57
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -72,7 +72,10 @@ const websitesToBlock = [
     "telegraph.co.uk",
     "i24news.tv",
     "kyivpost.com",
-    "cnbc.com"
+    "cnbc.com",
+    "youtube.com",
+    "twitter.com",
+    "jns.org"
 ]
 
 const annoyances = [
