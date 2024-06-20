487095774b1c8e88f3ffda946186f81de164b811
usernomom <148295218+usernomom@users.noreply.github.com>
2024-06-20 18:38:47 -0400

    Update instacart_ad_remover.js

--- instacart_ad_remover.js
+++ instacart_ad_remover.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Instacart Ad Remover
-// @version  39
+// @version  40
 // @match    https://*.instacart.ca/*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -15,7 +15,7 @@ unsafeWindow.Element.prototype.attachShadow = function () {
   });
 };
 
-let sponsoredText = "spawhnserd"
+let sponsoredText = "spaahnserd"
 
 function isSponsored(elem) {
   if (elem) {
