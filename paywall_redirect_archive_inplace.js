67db863a6f72c8b7f1ae49ed5316b473e878d60e
usernomom <148295218+usernomom@users.noreply.github.com>
2024-07-21 14:31:52 -0400

    y

--- paywall_redirect_archive_inplace.js
+++ paywall_redirect_archive_inplace.js
@@ -1,16 +1,25 @@
 // ==UserScript==
 // @name         Bypass paywalls using archive.today snapshot
+// @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/paywall_redirect_archive_inplace.js
 // @author       nobody
 // @description  Bypass WSJ paywall by redirecting to archive.is snapshot
-// @version      0.0.1
+// @version      2
 // @include      /^https://www\.wsj\.com/
+// @match        https://www.ft.com/*
 // ==/UserScript==
 
 function replaceLinks() {
   var links = document.getElementsByTagName('a');
-  var regexFull = /^https:\/\/www\.wsj\.com\/(.*)(\?.*)$/i;
+  var regexes = {
+    'https://www.wsj.com': /^https:\/\/www\.wsj\.com\/(.*)(\?.*)$/i,
+    'https://www.ft.com/content': /^https:\/\/www\.ft\.com\/content\/(.*)$/i
+  }
   for (var i = 0; i < links.length; i++) {
-    links[i].href = links[i].href.replace(regexFull, 'https://archive.ph/newest/https://www.wsj.com/$1');
+    for (const [source, regex] of Object.entries(regexes)) {
+      if (links[i].href.startsWith(source)) {
+        links[i].href = links[i].href.replace(regex, `https://archive.is/newest/${source}/$1`)
+      }
+    }
   }
 }
 
