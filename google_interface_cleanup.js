3faec97950a206bfe1b035531e70be95b6a1edde
usernomom <148295218+usernomom@users.noreply.github.com>
2024-07-31 08:14:40 -0400

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      68
+// @version      69
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -85,8 +85,8 @@ const websitesToBlock = [
     "pravda.com.ua",
     "nytimes.com",
     "msn.com",
-    "inc.com"
-    
+    "inc.com",
+    "voanews.com"
     
 ]
 
