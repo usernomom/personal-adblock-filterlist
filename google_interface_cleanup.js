19a62094c06ce220ccf232b547023876209ef921
usernomom <148295218+usernomom@users.noreply.github.com>
2024-08-16 14:19:40 -0400

    Update google_interface_cleanup.js

--- google_interface_cleanup.js
+++ google_interface_cleanup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Google interface cleanup
-// @version      76
+// @version      77
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/google_interface_cleanup.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -117,7 +117,8 @@ const annoyances = [
     'Events',
     'Profiles',
     'Perspectives',
-    'What to watch'
+    'What to watch',
+    'Posts on X'
 ]
 
 // Where el is the DOM element you'd like to test for visibility
