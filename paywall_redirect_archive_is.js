4c483400e6cc87b4685b0a5fff85c423d17def90
usernomom <148295218+usernomom@users.noreply.github.com>
2024-10-05 18:34:05 -0400

    Update paywall_redirect_archive_is.js

--- paywall_redirect_archive_is.js
+++ paywall_redirect_archive_is.js
@@ -2,16 +2,14 @@
 // @name         Bypass paywalls by redirecting to archive.is
 // @author       nobody
 // @description  Bypass paywalls by redirecting to archive.is
-// @version      5
+// @version      6
 // @downloadURL  https://raw.githubusercontent.com/usernomom/personal-adblock-filterlist/main/paywall_redirect_archive_is.js
 // @include      /^https://www\.wsj\.com/.+$/
 // @include      /^https://www\.thestar\.com/.+$/
 // @match        https://*.ft.com/content/*
 // @match        https://*.bloomberg.com/*/articles/*
 // @match        https://*.haaretz.com/*
-// @match        https://*.medium.com/*
 // @match        https://*.economist.com/*
-// @match        https://*.washingtonpost.com/foo*
 // @run-at       document-start
 // ==/UserScript==
 
