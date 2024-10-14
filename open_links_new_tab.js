49212657dbba8893ad2fa5a3327393f4fa1610c8
usernomom <148295218+usernomom@users.noreply.github.com>
2024-10-14 13:55:30 -0400

    t

--- open_links_new_tab.js
+++ open_links_new_tab.js
@@ -1,26 +1,26 @@
 // ==UserScript==
-// @name         Open All Links in New Tab
-// @namespace    http://tampermonkey.net/
-// @version      3.5.6
-// @author       Firey Chicken
+// @name         Google search - Open cross-site links in new tab
+// @version      2
 // @match        https://*.google.com/search*
 // @match        https://*.google.ca/search*
 // @match        https://*.google.fr/search*
 // @match        https://*.google.co.uk/search*
-// @grant       GM_addStyle
+// @grant        GM_addStyle
 // @grant        window.close
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
 })();
 var $ = window.jQuery;
-$(document).on('click', 'a', function(e){
+$(document).on('click', 'a', function (e) {
     e.preventDefault();
     var url = $(this).attr('href');
-    window.open(url, '_blank');
+    if (!url.startsWith('/')) {
+        window.open(url, '_blank');
+    }
 });
